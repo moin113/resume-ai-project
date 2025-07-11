c42664abb544d4b2379dd635e62df6ff4ff3468d
azamadmin <dev.azam001@gmail.com>
2025-07-11 23:12:26 +0530

    Update models.py

--- backend/resume-matcher-api/app/models.py
+++ backend/resume-matcher-api/app/models.py
@@ -1,1 +1,13 @@
+from pydantic import BaseModel
+from typing import List
+
+class MatchRequest(BaseModel):
+    job_description: str
+    resume: str
+
+class MatchResponse(BaseModel):
+    match_score: float
+    matched_keywords: List[str]
+    missing_keywords: List[str]
+    all_keywords: List[str]
 
